6d03ea3eac6ee193c4551046f3cef1b62e9bc8af
nerdalertdk <nerdalertdk@users.noreply.github.com>
2024-04-30 01:12:30 +0200

    Delete rename.sh

--- rename.sh
+++
@@ -1,8 +0,0 @@
-# for filename in repo/*/*; do 
-#     echo $filename
-#     [ -f "$filename" ] || continue
-#     mv "$filename" "${filename//dk_/}"
-# done
-
-
-for f in repo/*/*; do mv "$f" "$f.tmp"; mv "$f.tmp" "`echo $f | tr "[:upper:]" "[:lower:]"`"; done
